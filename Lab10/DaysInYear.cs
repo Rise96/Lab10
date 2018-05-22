e980f5f0fa2edbaf589e555f17e6c0521cd2c3b6
Rise <ritm-temp@hotmail.com>
2018-05-22 10:26:36 +0600

    Lab 10 v1.0

---
+++ Lab10/DaysInYear.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab10
+{
+    class DaysInYear
+    {
+        public int NumberOfDaysInYear(string yearString)
+        {
+            int year = Convert.ToInt32(yearString);
+            int days = 365;
+            if ((year % 4) == 0)
+            {
+                if ((year % 400) == 0)
+                {
+                    days++;
+                }
+            }
+
+            return days;
+        }
+    }
+}
---
+++ Lab10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DaysInYear yearConverter = new DaysInYear();
+            string year;
+            using (StreamReader reader = File.OpenText("input.txt"))
+            {
+                year = reader.ReadLine();
+            }
+            int days= yearConverter.NumberOfDaysInYear(year);
+            using (StreamWriter writer = File.CreateText("ouput.txt"))
+            {
+                writer.WriteLine($"Число дней в {year} году равно {days}");
+            }
+        }
+    }
+
+ }
